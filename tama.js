2bf70db2ddfd4704c499c8ff054ae548a4ffe317
thesneilert <122097231+thesneilert@users.noreply.github.com>
2023-02-01 11:07:16 +0100

    fix

--- tama.js
+++ tama.js
@@ -1,6 +1,6 @@
-// model
+// ----- MODEL -----
 
-// view
+// ----- VIEW -----
 const app = document.getElementById("app");
 
 updateView();
@@ -13,15 +13,16 @@ function updateView() {
      <div id = "progressbar1">innerhtml<div>
      <div id = "progressbar2">innerhtml<div>
      <div id = "progressbar3">innerhtml<div>
-     <button id = "btn1">Snacks</button>
-     <button id = "btn2">Spille Gitar</button>
-     <button id = "btn3">Sove</button>
+     <button id = "btn1" onclick="Eat">Snacks</button>
+     <button id = "btn2" onclick="Guitar">Spille Gitar</button>
+     <button id = "btn3" onclick="Sleep">Sove</button>
      </div>
      `;
 }
 
-// controller
+// ----- CONTROLLER -----
 
+//progressbar1
 function update() {
   var element = document.getElementById("progressbar1");
   var width = 100;
@@ -36,4 +37,5 @@ function update() {
       element.innerHTML = width * 1 + "%";
     }
   }
+  updateView();
 }
