6b9b13da13e084ba6718a4d1093c6cf992cbbbcd
SebastianWaage <122272875+SebastianWaage@users.noreply.github.com>
2023-02-01 10:38:42 +0100

    1231313123

--- tama.js
+++ tama.js
@@ -1,10 +1,14 @@
 // model
-const app = document.getElementById("app");
 
 // view
+const app = document.getElementById("app");
 updateView();
 function updateView() {
-  document.getElementById("app").innerHTML<div>
+  document.getElementById("app").innerHTML = /*html*/ `
+     <div class = "header"></div>
+     <div class = "dynamiskBenji"></div>
+     <div class = "tracker">
+     <div id = "progressbar1"><div>
      <button id = "btn1"></button>
      <button id = "btn2"></button>
      <button id = "btn3"></button>
@@ -15,7 +19,10 @@ function updateView() {
 // controller
 
 function update() {
-  va
+  var element = document.getElementById("progressbar1");
+  var width = 100;
+  var identity = setInterval(scene, 100);
+
   function scene() {
     if (width < 1) {
       clearInterval(identity);
@@ -26,4 +33,3 @@ function update() {
     }
   }
 }
-kyhgfkhjgfvjhgvhjgcvjhgcvhngvhjgv;
