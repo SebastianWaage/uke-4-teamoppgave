fcf875bf7d7616e1b459c844f091ef5808866284
thesneilert <122097231+thesneilert@users.noreply.github.com>
2023-02-01 12:07:09 +0100

    Made "progressbar1" and "btn1" functions and animations
    The bar wil now remove 1% of width every 1,5 second until it hits 0%. When pressing the "popkorn" button you it will add 10% every click until it hits 100%. Thats the cap.

--- tama.js
+++ tama.js
@@ -15,49 +15,34 @@ function updateView() {
     <div class = "header">Benjigotchi</div>
     <div class = "dynamiskBenji">bildet av benji</div>
       <div class = "tracker">
-      <div id = "progressbar1">${progressbar1}<div>
+      <div id="progressbar1" style="width:${progressbar1/100*300}px">${progressbar1}%</div>
+      <!-- capper størelse på progressbaren med (style="width:${progressbar1/100*300}px") til 300px. -->
       <div id = "progressbar2">${progressbar2}<div>
       <div id = "progressbar3">${progressbar3}<div>
       </div>
-    <button id = "btn1" onclick = "Eat()">Popcorn</button>
-    <button id = "btn2" onclick = "Guitar()">Spille Gitar</button>
-    <button id = "btn3" onclick = "Sleep()">Sove</button>
+    <button id = "btn1" onclick = "eat()">Popkorn</button>
+    <button id = "btn2" onclick = "guitar()">Spille Gitar</button>
+    <button id = "btn3" onclick = "sleep()">Sove</button>
     `;
 }
 
 // -------------------- CONTROLLER --------------------
-function updateProgressbar1() {
-  if (progressbar1 > 0) {
-    progressbar1--;
-    updateView();
-    setTimeout(updateProgressbar1, 1000);
+
+//animasjon til progressbar1
+function updateProgressbar1() { //trekker fra 1 tall per 1,5 sekund fra progressbar1
+  if (progressbar1 > 0) { //setter nedre grense på 0%, den stopper å fjerne 1% ved 0%
+    progressbar1--; //fjerner 1%
+    updateView(); //oppdaterer siden
+    setTimeout(updateProgressbar1, 1000); //setter refresh på 1,5 sekund
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-//progressbar1
-//function update(){
-//  var element = document.getElementById("progressbar1");
-//  var width = 100;
-//  var identity = setInterval(scene, 100);
-//
-//  function scene() {
-//    if (width < 1) {
-//      clearInterval(identity);
-//    } else {
-//      width--;
-//      element.style.width = width + "%";
-//      element.innerHTML = width * 1 + "%";
-//    }
-//  }
-//}
+//funksjonen til btn1 "popkorn"
+function eat(){ //legger til 10% når man trykker på "popkorn"
+  if (progressbar1 < 90) { //gjør så man ikke kan gå over 100% ved å klikke.
+    progressbar1 = progressbar1 + 10; //leger til 10%
+  } else { //vis ikke
+    progressbar1 = 100; //setter grense/cap på 100%
+  }
+  updateView();
+}
